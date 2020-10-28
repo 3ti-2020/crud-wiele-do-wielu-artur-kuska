de2e0f0b9b9ee0175323561e9bcf77695758fe03
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2020-10-28 11:16:37 +0100

    dodanie id + stylistyka tabeli

--- index.php
+++ index.php
@@ -11,31 +11,35 @@
 
     <header><h1>Artur Kuśka 4Ti gr 1</h1></header>
     <main>
-    <?php
-        $servername = "sql7.freemysqlhosting.net";
-        $username = "sql7373164";
-        $password = "XblNhVncck";
-        $dbname = "sql7373164";
+        <div class="tab">
+        <?php
+            $servername = "sql7.freemysqlhosting.net";
+            $username = "sql7373164";
+            $password = "XblNhVncck";
+            $dbname = "sql7373164";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        $result = $conn->query("SELECT lib_autor.autor, lib_tytul.tytul FROM lib_autor_tytul JOIN lib_tytul on lib_autor_tytul.id_tytul = lib_tytul.id_tytul JOIN lib_autor on lib_autor_tytul.id_autor = lib_autor.id_autor");
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            $result = $conn->query("SELECT lib_autor_tytul.id_autor_tytul as id, lib_autor.autor, lib_tytul.tytul FROM lib_autor_tytul JOIN lib_tytul on lib_autor_tytul.id_tytul = lib_tytul.id_tytul JOIN lib_autor on lib_autor_tytul.id_autor = lib_autor.id_autor");
 
-        echo("<table class='tabelka' border=1>");
-            echo("<tr>
-            <th>Nazwisko</th>
-            <th>Tytul</th>
-            </tr>");
+            echo("<table class='tabelka' border=1>");
+                echo("<tr>
+                <th class='lp'>ID</th>
+                <th>Nazwisko</th>
+                <th>Tytul</th>
+                </tr>");
 
-            while($row=$result->fetch_assoc() ){
-                echo("<tr>");
-                echo("<td>".$row['autor']."</td>");
-                echo("<td>".$row['tytul']."</td>");
-                echo("</tr>");
-            }
+                while($row=$result->fetch_assoc() ){
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td>");
+                    echo("<td>".$row['autor']."</td>");
+                    echo("<td>".$row['tytul']."</td>");
+                    echo("</tr>");
+                }
 
-        echo("</table>");
-        
-    ?>
+            echo("</table>");
+            
+        ?>
+        </div>
 </main>
 
 <nav>

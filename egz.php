6fe408aa7f1b81a18c85f5115dceef9f3819c60d
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2020-12-08 12:00:12 +0100

    Add files via upload

---
+++ egz.php
@@ -0,0 +1,67 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cont">
+        <div class="head">
+            <h1>Pytania z egazminów EE08/E14</h1>
+        </div>
+        <div class="main">
+
+            <article class="question">
+                <h3 class="question_title">Aby obraz zmieniał się płynnie w filmie, liczba klatek (nieprzenikających się wzajemnie) na sekundę musi znajdować się przynajmniej w zakresie</h3>
+                <ol type="A" class="question_answerList">
+                <li class="question_answerItem">31-36 fps</li>
+                <li class="question_answerItem">24-30 fps</li>
+                <li class="question_answerItem">16-19 fps</li>
+                <li class="question_answerItem">20-23 fps</li>
+                <details class="question_details">
+                    <summary class="question_detailsSummary">Sprawdź odpowiedź</summary> 
+                    <p class="question_detailsRightAnswer"> Odpowiedz: 
+                    <span>B</span> 
+                    </p>
+                </details>
+                </ol>
+            </article>
+
+            <article class="question">
+            <h3 class="question_title">Grafik chce przekształcić bez utraty jakości obraz JPG w format PNG w ten sposób, żeby wszędzie tam, gdzie w pierwotnym obrazie jest kolor biały, w obrazie docelowym była przezroczystość. Aby to zrobić, powinien</h3>
+            <ol type="A" class="question_answerList">
+                <li class="question_answerItem">dodać kanał alfa</li>
+                <li class="question_answerItem">zmniejszyć rozdzielczość obrazu</li>
+                <li class="question_answerItem">zaimportować obraz do edytora grafiki wektorowej</li>
+                <li class="question_answerItem">przekształcić obraz w odcienie szarości</li>
+                <details class="question_details">
+                <summary class="question_detailsSummary">sprawdź odpowiedź</summary> 
+                <p class="question_detailsRightAnswer"> Odpowiedz: 
+                    <span>A</span> 
+                </p>
+                </details>
+            </ol>
+            </article>
+
+            <!-- <article class="question">
+            <h3 class="question_title"></h3>
+            <ol type="A" class="question_answerList">
+                <li class="question_answerItem"></li>
+                <li class="question_answerItem"></li>
+                <li class="question_answerItem"></li>
+                <li class="question_answerItem"></li>
+                <details class="question_details">
+                <summary class="question_detailsSummary">Sprawdź odpowiedź</summary> 
+                <p class="question_detailsRightAnswer"> Odpowiedz: 
+                    <span></span> 
+                </p>
+                </details>
+            </ol>
+            </article> -->
+
+        </div>
+
+    </div>
+</body>
+</html>

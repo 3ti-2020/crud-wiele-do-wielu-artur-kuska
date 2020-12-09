7193ccbe80e43deb56a583542809bed8656baf05
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2020-12-09 19:11:48 +0100

    Add files via upload

---
+++ post.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="all">
+        <div class="content">
+            <form class="form">
+                <label>Podaj zadanie</label>
+                <textarea id="text-area" class="text-area" ></textarea>
+                <input type="submit" value="Dodaj">
+            </form>
+        </div>
+        <div class="list">
+            <template class="task-cont">
+                <div class="task-text"></div>
+            </template>
+        </div>
+
+    </div>
+
+</body>
+    <script src="jotes.js"></script>
+</html>
